59d0f44a63ee25e53fd9a5aeb439bdb2d59db427
TimoLischke <timo.lischke@gmx.net>
2025-01-29 15:58:08 +0100

    updated

--- lab-sql-aggregation-and-transformation.sql
+++ lab-sql-aggregation-and-transformation.sql
@@ -9,6 +9,7 @@ SELECT MIN(length) AS min_duration FROM film;
 -- Don't use decimals. Hint: Look for floor and round functions.
 SELECT ROUND(AVG(length)) AS avg_duration_in_min FROM film;
 SELECT CONCAT(LPAD(FLOOR(ROUND(AVG(length)) / 60), 2, '0'), ':', LPAD(ROUND(AVG(length)) % 60, 2, '0')) AS avg_duration_formatted FROM film;
+SELECT CONCAT(FLOOR(AVG(length) / 60), ':', (ROUND(AVG(length) % 60))) AS avg_duration_formatted FROM film;
 -- 2. You need to gain insights related to rental dates:
 -- 2.1 Calculate the *number of days that the company has been operating*.
 -- Hint: To do this, use the `rental` table, and the `DATEDIFF()` function
@@ -17,15 +18,17 @@ SELECT DATEDIFF(MAX(rental_date), MIN(rental_date)) AS operating_days FROM renta
 -- 2.2 Retrieve rental information and add two additional columns to show
 -- the **month and weekday of the rental**. Return 20 rows of results.
 SELECT *, MONTH(rental_date) AS month_of_rental, DAYOFWEEK(rental_date) AS weekday_of_rental FROM rental LIMIT 20;
+SELECT *, MONTHNAME(rental_date) AS month_of_rental, DAYNAME(rental_date) AS weekday_of_rental FROM rental LIMIT 20;
 -- 2.3 Bonus: Retrieve rental information and add an additional column
 -- called `DAY_TYPE` with values **'weekend' or 'workday'**, 
 -- depending on the day of the week.* Hint: use a conditional expression.
+-- DAYOFWEEK: 1 and 7 are weekend days, 1 = Sunday, 7 = Saturday
 SELECT *, 
 	CASE
-		WHEN DAYOFWEEK(rental_date) < 6 THEN "weekday"
+		WHEN DAYOFWEEK(rental_date) in (1,7) THEN "weekday"
         ELSE "weekend"
 	END AS weekday_of_rental
-FROM rental
+FROM rental;
 -- 3. Ensure that customers can easily access information about movie collection. 
 -- To achieve this, retrieve the **film titles and their rental duration**. 
 -- If any rental duration value is NULL, replace it with string 'Not Available'.
@@ -33,4 +36,33 @@ FROM rental
 -- *Please note: even if there are currently no null values in rental duration column, 
 -- the query should still be written to handle such cases in the future.*
 -- *Hint: Look for the `IFNULL()` function.*
-SELECT inventory_id, DATEDIFF(return_date, rental_date) AS rental_duration FROM rental
+SELECT title,
+	CASE
+		WHEN ISNULL(rental_duration) THEN "Not Available"
+        ELSE rental_duration
+        END AS rental_duration
+FROM film;
+
+-- 4. *Bonus: The marketing team for the movie rental company now needs to create a personalized email campaign for customers. To achieve this, you need to retrieve the **concatenated first and last names of customers**, along with the **first 3 characters of their email** address, so that you can address them by their first name and use their email address to send personalized recommendations. 
+-- The results should be ordered by last name in ascending order to make it easier to use the data.*
+
+
+-- Challenge 2: Next, you need to analyze the films in the collection to gain some more insights. 
+-- Using the `film` table, determine:
+-- 1.1 The **total number of films** that have been released.
+SELECT release_year, COUNT(film_id) AS nr_films FROM film GROUP BY release_year ORDER BY release_year;
+
+-- 1.3 The **number of films for each rating, sorting** the results in descending order of the number of films.
+-- This will help you to better understand the popularity of different film ratings and adjust purchasing decisions accordingly.
+SELECT rating, SUM(film_id) AS num_films FROM film GROUP BY rating ORDER BY SUM(film_id) DESC;
+
+-- 2. Using the `film` table, determine:
+-- 2.1 The **mean film duration for each rating**, and sort the results in descending order of the mean duration. 
+-- Round off the average lengths to two decimal places. 
+-- This will help identify popular movie lengths for each category.
+SELECT rating, ROUND(AVG(length), 2) FROM film GROUP BY rating ORDER BY AVG(length) DESC;
+-- 2.2 Identify **which ratings have a mean duration of over two hours** 
+-- in order to help select films for customers who prefer longer movies.
+SELECT rating, AVG(length) AS avg_length FROM film GROUP BY rating HAVING AVG(length) > 120
+
+-- 3. *Bonus: determine which last names are not repeated in the table `actor`.*
